d193ac292d726d981628c9892a22e2a64efebd17
ZeroGalack <os100donos@gmail.com>
2024-03-30 13:14:46 -0300

    .

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
+
+# Criar a aplicação Flask
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World!"
+@app.route('/', methods=['GET','POST'])
+def teste():
+    return "teste"
 
 if __name__ == '__main__':
     app.run(debug=True)
